7321e23b782271b454984dc2b9cabe69b6e8b80f
smiernik1 <smiernik@visa.com>
2026-04-01 11:21:51 +0200

    Initial project setup with Spring Boot and database configuration

---
+++ MealPlannerAPI/src/test/java/pl/coderslab/mealplannerapi/MealPlannerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.mealplannerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealPlannerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
